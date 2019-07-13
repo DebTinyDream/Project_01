dacba1b749c78daa243eae8ecc40f635ad9a420d
DebTinyDream <mdebpaikar@outlook.com>
2019-07-14 01:59:32 +0530

    LefT View Order Traversal Inspired from GeeksForGeeks

--- Left_view_Tree.cpp
+++ Left_view_Tree.cpp
@@ -21,13 +21,28 @@ struct BinaryTree** insert(struct BinaryTree** ptr, int data_insert)
 	*ptr = ptrToNew ;
 	return ptr;
 }
-void printTree(struct BinaryTree* PtrHead)
+void leftViewUtil(struct BinaryTree* PtrHead, int level, int *max_level)
 {
-	while(PtrHead)
+	if(!PtrHead)
+		return;
+	if(*max_level < level)
+	{
+		cout<<PtrHead->data<<" ";
+		*max_level = level;
+	}
+	leftViewUtil(PtrHead->left,level+1,max_level);
+	leftViewUtil(PtrHead->right, level + 1, max_level);
+}
+void leftView(struct BinaryTree* PtrHead)
+{
+	int max_level=0;
+	leftViewUtil(PtrHead,1,&max_level);
+	/*while(PtrHead)
 	{
 		cout<<"====="<<PtrHead->data<<"====\n";
 		PtrHead = PtrHead->left;
-	}
+	}*/
+	
 }
 int main()
 {
@@ -35,11 +50,17 @@ int main()
 	insert(&PtrHead,10);
 	insert(&PtrHead->left,20);
 	insert(&PtrHead->right,30);
-	insert(&PtrHead->left->left,20);
-	insert(&PtrHead->left->right,20);
-	insert(&PtrHead->right->left,30);
-	insert(&PtrHead->right->right,30);
-	printTree(PtrHead);
+	//insert(&PtrHead->left->left,20);
+	insert(&PtrHead->left->right,40);
+	//insert(&PtrHead->left->right,20);
+	insert(&PtrHead->right->left,50);
+	insert(&PtrHead->right->right,60);
+	insert(&PtrHead->right->left->right,70);
+	insert(&PtrHead->right->left->left,80);
+	insert(&PtrHead->right->left->left->left,90);
+	insert(&PtrHead->right->left->left->right,100);
+	
+	leftView(PtrHead);
 	
 }
 
